9e16b01410c06e979b930cb60d2951659fb38214
MiniDaddy <58199478+MiniDaddy@users.noreply.github.com>
2023-04-18 16:56:08 +0900

    Add files via upload

---
+++ chap06/chap06/src/chap06/EnumWeek.java
@@ -0,0 +1,18 @@
+package chap06;
+
+public class EnumWeek {
+
+	public enum Week{
+		MONDAY, //결거 상수(대문자)
+		TUESDAY,
+		WEDNESADY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+			
+			
+			
+	}
+
+}
